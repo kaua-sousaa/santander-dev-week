598366efd6106663118a9a563264caf9c5dff7fe
kaua-sousaa <kauasousa15_rj@hotmail.com>
2024-07-17 01:04:31 -0300

    corrigindo cors

--- santander-dev-week/src/main/java/me/dio/SantanderDevWeekApplication.java
+++ santander-dev-week/src/main/java/me/dio/SantanderDevWeekApplication.java
@@ -1,10 +1,13 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderDevWeekApplication {
 
